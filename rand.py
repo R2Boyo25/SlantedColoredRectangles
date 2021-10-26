a07397040249e568e88689182d95e75692dad80d
R2boyo25 <r2boyo25@gmail.com>
2021-10-26 18:11:25 -0500

    idk

--- rand.py
+++ rand.py
@@ -1,9 +1,30 @@
-import math
+import math, random
 from PIL import Image, ImageDraw, ImageFilter
 
 colors = ['#FFFFFF', "#EFEFEF", "#c0ffee"]
 
 ang = 10
+
+def add(onum, num, max):
+    print(max)
+    if onum + num >= max:
+        print(max, True)
+        return max, True
+    else:
+        print(onum + num, False)
+        return onum + num, False
+
+def nextColor(cnum, colors):
+    if cnum + 1 >= len(colors):
+        print(colors[0], 0)
+        return colors[0], 0
+    else:
+        print(colors[cnum + 1], cnum + 1)
+        return colors[cnum + 1], cnum + 1
+
+def randLength(sqmin, sqmax, last, screenMax):
+    print(add(last, random.randint(sqmin, sqmax), screenMax))
+    return add(last, random.randint(sqmin, sqmax), screenMax)
 
 def polx(dr, s, i, wh, ang, color):
     h = wh[1]
@@ -50,10 +71,21 @@ def poly(dr, s, i, wh, ang, color):
             ), fill = color)
 
 def horizontal(w, h):
-
     img = Image.new("RGB", (w*10, h*10))
     dr = ImageDraw.Draw(img)
+    lst, smin, smax = 0, ang*2, math.ceil(w/len(colors))
+    ccolor = ('ffffff', -1)
 
+    while not lst >= w/10:
+        ccolor = nextColor(ccolor[1], colors)
+        llst = lst
+        lst = randLength(smin, smax/10, lst, w/10)[0]
+        #lst = randLength(smin, smax/10, lst, w/10)[0]
+        polx(dr, llst, lst, (w, h), ang, ccolor)
+        
+
+
+    '''
     s = ((w / len(colors)) * -1)
     wi = w / len(colors) + s
     polx(dr, s, wi, (w, h), ang, colors[-1])
@@ -66,6 +98,7 @@ def horizontal(w, h):
     s = ((w / len(colors)) * len(colors)-1)
     wi = w / len(colors) + s
     polx(dr, s, wi, (w, h), ang, colors[0])
+    '''
 
     nimg = img.resize((w, h), resample=Image.ANTIALIAS)
 
